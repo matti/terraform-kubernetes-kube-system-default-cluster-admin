65ce7943b26fdcc588d5c09e73c48f3702bd5d29
Matti Paksula <matti.paksula@iki.fi>
2019-08-23 09:44:51 +0300

    :sunrise:

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "kubernetes_cluster_role_binding" "default" {
+
+  metadata {
+    name = "kube-system-default-cluster-admin"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "default"
+    namespace = "kube-system"
+  }
+}
